9753acb3f71f4745dfae2eb372010b05f9e55b21
gurum77 <hileejaeho@gmail.com>
2020-06-10 01:00:14 +0900

    하단 버튼 추가 / 카메라 / 갤러리 연결

--- lib/PhotoInputPage.dart
+++ lib/PhotoInputPage.dart
@@ -1,3 +1,5 @@
+import 'dart:io';
+import 'package:image_picker/image_picker.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:small_biz/ReceiptData.dart';
@@ -17,6 +19,20 @@ class _PhotoInputPageState extends State<PhotoInputPage> {
   ReceiptDataHelper receiptDataHelper = ReceiptDataHelper();
   ReceiptData receiptData = ReceiptData();
   List<DataRow> rows = new List<DataRow>();
+  File _image;
+  final picker = ImagePicker();
+
+  Future getImage(bool byCamera) async {
+    final pickedFile = await picker.getImage(
+        source: byCamera ? ImageSource.camera : ImageSource.gallery);
+
+    setState(() {
+      if (pickedFile == null)
+        _image = null;
+      else
+        _image = File(pickedFile.path);
+    });
+  }
 
   void AddDataRow(List<DataRow> rows, ReceiptDataType type) {
     if (!receiptDataHelper.isVisible(type)) return;
@@ -51,6 +67,24 @@ class _PhotoInputPageState extends State<PhotoInputPage> {
     return MaterialApp(
         title: 'PhotoInput',
         home: Scaffold(
+          bottomNavigationBar: BottomNavigationBar(
+              onTap: _onItemTapped,
+              type: BottomNavigationBarType.fixed,
+              items: <BottomNavigationBarItem>[
+                BottomNavigationBarItem(
+                    icon: Icon(Icons.skip_previous), title: Text('Prev.')),
+                BottomNavigationBarItem(
+                    icon: Icon(Icons.save), title: Text('Save')),
+                BottomNavigationBarItem(
+                    icon: Icon(Icons.skip_next), title: Text('Next')),
+                BottomNavigationBarItem(
+                    icon: Icon(Icons.space_bar), title: Text('')),
+                BottomNavigationBarItem(
+                    icon: Icon(Icons.add), title: Text('New')),
+                BottomNavigationBarItem(
+                    icon: Icon(Icons.photo), title: Text('Photo')),
+                // BottomNavigationBarItem(icon: Icon(Icons.add), title: Text('Add')),
+              ]),
           body: Container(
               // decoration: BoxDecoration(
               //     image: DecorationImage(
@@ -64,11 +98,21 @@ class _PhotoInputPageState extends State<PhotoInputPage> {
               ], rows: this.rows),
             ],
           )),
-          floatingActionButton: FloatingActionButton(
-            onPressed: null,
-            child: Icon(Icons.add),
-            elevation: 20,
-          ),
+          // floatingActionButton: FloatingActionButton(
+          //   onPressed: null,
+          //   child: Icon(Icons.add),
+          //   elevation: 20,
+          // ),
         ));
+  }
+
+  void _onItemTapped(int value) {
+    if (value == 4) {
+      // camera
+      getImage(true);
+    } else if (value == 5) {
+      // gallery
+      getImage(false);
+    }
   }
 }
